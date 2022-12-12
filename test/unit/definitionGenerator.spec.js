40caf575b5ccf2d24fb2051ac316378adcb592e5
JaredAAT <jarede@gmail.com>
2022-12-12 13:19:08 +0000

    create specs for security schemes

--- test/unit/definitionGenerator.spec.js
+++ test/unit/definitionGenerator.spec.js
@@ -3,7 +3,7 @@
 const fs = require('fs').promises
 const path = require('path')
 const sinon = require('sinon')
-const $RefParser = require("@apidevtools/json-schema-ref-parser");
+const $RefParser = require("@apidevtools/json-schema-ref-parser")
 const expect = require('chai').expect
 
 const serverlessMock = require('../helpers/serverless')
@@ -268,6 +268,380 @@ describe('DefinitionGenerator', () => {
             expect(definitionGenerator.openAPI).to.be.an('object')
             expect(definitionGenerator.openAPI.info).to.be.an('object')
             expect(definitionGenerator.openAPI.info).to.not.have.property('otherField')
+        });
+    });
+
+    describe('createSecuritySchemes', () => {
+        describe('API Keys', () => {
+            it('should add an API Key security scheme to components', function() {
+                mockServerless.service.custom.documentation.securitySchemes = {
+                    'api_key': {
+                        type: 'apiKey',
+                        name: 'Authorization',
+                        in: 'header'
+                    }
+                }
+
+                const definitionGenerator = new DefinitionGenerator(mockServerless)
+                definitionGenerator.createSecuritySchemes(mockServerless.service.custom.documentation.securitySchemes)
+
+                expect(definitionGenerator.openAPI).to.be.an('object')
+                expect(definitionGenerator.openAPI.components).to.be.an('object')
+                expect(definitionGenerator.openAPI.components).to.have.property('securitySchemes')
+                expect(definitionGenerator.openAPI.components.securitySchemes).to.be.an('object')
+                expect(definitionGenerator.openAPI.components.securitySchemes).to.have.property('api_key')
+                expect(definitionGenerator.openAPI.components.securitySchemes.api_key).to.have.property('type')
+                expect(definitionGenerator.openAPI.components.securitySchemes.api_key.type).to.be.equal('apiKey')
+            });
+
+            it('should throw an error when name is missing from an API Key scheme', function() {
+                mockServerless.service.custom.documentation.securitySchemes = {
+                    'api_key': {
+                        type: 'apiKey',
+                        in: 'header'
+                    }
+                }
+
+                const definitionGenerator = new DefinitionGenerator(mockServerless)
+                expect(() => {
+                    definitionGenerator.createSecuritySchemes(mockServerless.service.custom.documentation.securitySchemes)
+                }).to.throw('Security Scheme for "apiKey" requires the name of the header, query or cookie parameter to be used')
+            });
+
+            it('should throw an error when in is missing from an API Key scheme', function() {
+                mockServerless.service.custom.documentation.securitySchemes = {
+                    'api_key': {
+                        type: 'apiKey',
+                        name: 'Authorization',
+                    }
+                }
+
+                const definitionGenerator = new DefinitionGenerator(mockServerless)
+                expect(() => {
+                    definitionGenerator.createSecuritySchemes(mockServerless.service.custom.documentation.securitySchemes)
+                }).to.throw('Security Scheme for "apiKey" requires the location of the API key: header, query or cookie parameter')
+            });
+        });
+
+        describe('HTTP', () => {
+            it('should add an HTTP security scheme to components', function() {
+                mockServerless.service.custom.documentation.securitySchemes = {
+                    'http_key': {
+                        type: 'http',
+                        scheme: 'basic'
+                    }
+                }
+
+                const definitionGenerator = new DefinitionGenerator(mockServerless)
+                definitionGenerator.createSecuritySchemes(mockServerless.service.custom.documentation.securitySchemes)
+
+                expect(definitionGenerator.openAPI).to.be.an('object')
+                expect(definitionGenerator.openAPI.components).to.be.an('object')
+                expect(definitionGenerator.openAPI.components).to.have.property('securitySchemes')
+                expect(definitionGenerator.openAPI.components.securitySchemes).to.be.an('object')
+                expect(definitionGenerator.openAPI.components.securitySchemes).to.have.property('http_key')
+            });
+
+            it('should throw an error when scheme is missing from an HTTP scheme', function() {
+                mockServerless.service.custom.documentation.securitySchemes = {
+                    'http_key': {
+                        type: 'http',
+                    }
+                }
+
+                const definitionGenerator = new DefinitionGenerator(mockServerless)
+                expect(() => {
+                    definitionGenerator.createSecuritySchemes(mockServerless.service.custom.documentation.securitySchemes)
+                }).to.throw('Security Scheme for "http" requires scheme')
+            });
+        });
+
+        describe('openIdConnect', () => {
+            it('should add an openIdConnect security scheme to components', function() {
+                mockServerless.service.custom.documentation.securitySchemes = {
+                    'openIdConnect_key': {
+                        type: 'openIdConnect',
+                        openIdConnectUrl: 'http://example.com'
+                    }
+                }
+
+                const definitionGenerator = new DefinitionGenerator(mockServerless)
+                definitionGenerator.createSecuritySchemes(mockServerless.service.custom.documentation.securitySchemes)
+
+                expect(definitionGenerator.openAPI).to.be.an('object')
+                expect(definitionGenerator.openAPI.components).to.be.an('object')
+                expect(definitionGenerator.openAPI.components).to.have.property('securitySchemes')
+                expect(definitionGenerator.openAPI.components.securitySchemes).to.be.an('object')
+                expect(definitionGenerator.openAPI.components.securitySchemes).to.have.property('openIdConnect_key')
+            });
+
+            it('should throw an error when openIdConnectUrl is missing from an openIdConnect scheme', function() {
+                mockServerless.service.custom.documentation.securitySchemes = {
+                    'openIdConnect_key': {
+                        type: 'openIdConnect',
+                    }
+                }
+
+                const definitionGenerator = new DefinitionGenerator(mockServerless)
+                expect(() => {
+                    definitionGenerator.createSecuritySchemes(mockServerless.service.custom.documentation.securitySchemes)
+                }).to.throw('Security Scheme for "openIdConnect" requires openIdConnectUrl')
+            });
+        });
+
+        describe('oauth2', () => {
+            it('should add an oauth2 security scheme to components', function() {
+                mockServerless.service.custom.documentation.securitySchemes = {
+                    'oAuth2_key': {
+                        type: 'oauth2',
+                        flows: {
+                            implicit: {
+                                authorizationUrl: 'http://example.org/api/oauth/dialog',
+                                scopes: {
+                                    'write:pets': 'modify pets in your account',
+                                    'read:pets': 'read your pets'
+                                }
+                            }
+                        }
+                    }
+                }
+
+                const definitionGenerator = new DefinitionGenerator(mockServerless)
+                definitionGenerator.createSecuritySchemes(mockServerless.service.custom.documentation.securitySchemes)
+
+                expect(definitionGenerator.openAPI).to.be.an('object')
+                expect(definitionGenerator.openAPI.components).to.be.an('object')
+                expect(definitionGenerator.openAPI.components).to.have.property('securitySchemes')
+                expect(definitionGenerator.openAPI.components.securitySchemes).to.be.an('object')
+                expect(definitionGenerator.openAPI.components.securitySchemes).to.have.property('oAuth2_key')
+            });
+
+            it('should throw an error when flows is missing from an oauth2 scheme', function() {
+                mockServerless.service.custom.documentation.securitySchemes = {
+                    'oAuth2_key': {
+                        type: 'oauth2',
+                    }
+                }
+
+                const definitionGenerator = new DefinitionGenerator(mockServerless)
+                expect(() => {
+                    definitionGenerator.createSecuritySchemes(mockServerless.service.custom.documentation.securitySchemes)
+                }).to.throw('Security Scheme for "oauth2" requires flows')
+            });
+
+            it('should throw an error when authorizationUrl is missing from an oauth2 implicit flow scheme', function() {
+                mockServerless.service.custom.documentation.securitySchemes = {
+                    'oAuth2_key': {
+                        type: 'oauth2',
+                        flows: {
+                            implicit: {
+                                scopes: {
+                                    'write:pets': 'modify pets in your account',
+                                    'read:pets': 'read your pets'
+                                }
+                            }
+                        }
+                    }
+                }
+
+                const definitionGenerator = new DefinitionGenerator(mockServerless)
+                expect(() => {
+                    definitionGenerator.createSecuritySchemes(mockServerless.service.custom.documentation.securitySchemes)
+                }).to.throw('oAuth2 implicit flow requires an authorizationUrl')
+            });
+
+            it('should throw an error when authorizationUrl is missing from an oauth2 authorizationCode flow scheme', function() {
+                mockServerless.service.custom.documentation.securitySchemes = {
+                    'oAuth2_key': {
+                        type: 'oauth2',
+                        flows: {
+                            authorizationCode: {
+                                tokenUrl: 'http://example.com',
+                                scopes: {
+                                    'write:pets': 'modify pets in your account',
+                                    'read:pets': 'read your pets'
+                                }
+                            }
+                        }
+                    }
+                }
+
+                const definitionGenerator = new DefinitionGenerator(mockServerless)
+                expect(() => {
+                    definitionGenerator.createSecuritySchemes(mockServerless.service.custom.documentation.securitySchemes)
+                }).to.throw('oAuth2 authorizationCode flow requires an authorizationUrl')
+            });
+
+            it('should throw an error when tokenUrl is missing from an oauth2 authorizationCode flow scheme', function() {
+                mockServerless.service.custom.documentation.securitySchemes = {
+                    'oAuth2_key': {
+                        type: 'oauth2',
+                        flows: {
+                            authorizationCode: {
+                                authorizationUrl: 'http://example.org/api/oauth/dialog',
+                                scopes: {
+                                    'write:pets': 'modify pets in your account',
+                                    'read:pets': 'read your pets'
+                                }
+                            }
+                        }
+                    }
+                }
+
+                const definitionGenerator = new DefinitionGenerator(mockServerless)
+                expect(() => {
+                    definitionGenerator.createSecuritySchemes(mockServerless.service.custom.documentation.securitySchemes)
+                }).to.throw('oAuth2 authorizationCode flow requires a tokenUrl')
+            });
+
+            it('should throw an error when tokenUrl is missing from an oauth2 password flow scheme', function() {
+                mockServerless.service.custom.documentation.securitySchemes = {
+                    'oAuth2_key': {
+                        type: 'oauth2',
+                        flows: {
+                            password: {
+                                scopes: {
+                                    'write:pets': 'modify pets in your account',
+                                    'read:pets': 'read your pets'
+                                }
+                            }
+                        }
+                    }
+                }
+
+                const definitionGenerator = new DefinitionGenerator(mockServerless)
+                expect(() => {
+                    definitionGenerator.createSecuritySchemes(mockServerless.service.custom.documentation.securitySchemes)
+                }).to.throw('oAuth2 password flow requires a tokenUrl')
+            });
+
+            it('should throw an error when tokenUrl is missing from an oauth2 clientCredentials flow scheme', function() {
+                mockServerless.service.custom.documentation.securitySchemes = {
+                    'oAuth2_key': {
+                        type: 'oauth2',
+                        flows: {
+                            clientCredentials: {
+                                scopes: {
+                                    'write:pets': 'modify pets in your account',
+                                    'read:pets': 'read your pets'
+                                }
+                            }
+                        }
+                    }
+                }
+
+                const definitionGenerator = new DefinitionGenerator(mockServerless)
+                expect(() => {
+                    definitionGenerator.createSecuritySchemes(mockServerless.service.custom.documentation.securitySchemes)
+                }).to.throw('oAuth2 clientCredentials flow requires a tokenUrl')
+            });
+
+            it('should throw an error when scopes is missing from an oauth2 clientCredentials flow scheme', function() {
+                mockServerless.service.custom.documentation.securitySchemes = {
+                    'oAuth2_key': {
+                        type: 'oauth2',
+                        flows: {
+                            clientCredentials: {
+                                tokenUrl: 'http://example.com',
+                            }
+                        }
+                    }
+                }
+
+                const definitionGenerator = new DefinitionGenerator(mockServerless)
+                expect(() => {
+                    definitionGenerator.createSecuritySchemes(mockServerless.service.custom.documentation.securitySchemes)
+                }).to.throw('oAuth2 clientCredentials flow requires scopes')
+            });
+
+            it('should throw an error when scopes is missing from an oauth2 authorizationCode flow scheme', function() {
+                mockServerless.service.custom.documentation.securitySchemes = {
+                    'oAuth2_key': {
+                        type: 'oauth2',
+                        flows: {
+                            authorizationCode: {
+                                tokenUrl: 'http://example.com',
+                                authorizationUrl: 'http://example.org/api/oauth/dialog',
+                            }
+                        }
+                    }
+                }
+
+                const definitionGenerator = new DefinitionGenerator(mockServerless)
+                expect(() => {
+                    definitionGenerator.createSecuritySchemes(mockServerless.service.custom.documentation.securitySchemes)
+                }).to.throw('oAuth2 authorizationCode flow requires scopes')
+            });
+
+            it('should throw an error when scopes is missing from an oauth2 password flow scheme', function() {
+                mockServerless.service.custom.documentation.securitySchemes = {
+                    'oAuth2_key': {
+                        type: 'oauth2',
+                        flows: {
+                            password: {
+                                tokenUrl: 'http://example.com',
+                            }
+                        }
+                    }
+                }
+
+                const definitionGenerator = new DefinitionGenerator(mockServerless)
+                expect(() => {
+                    definitionGenerator.createSecuritySchemes(mockServerless.service.custom.documentation.securitySchemes)
+                }).to.throw('oAuth2 password flow requires scopes')
+            });
+
+            it('should throw an error when scopes is missing from an oauth2 implicit flow scheme', function() {
+                mockServerless.service.custom.documentation.securitySchemes = {
+                    'oAuth2_key': {
+                        type: 'oauth2',
+                        flows: {
+                            implicit: {
+                                authorizationUrl: 'http://example.org/api/oauth/dialog',
+                            }
+                        }
+                    }
+                }
+
+                const definitionGenerator = new DefinitionGenerator(mockServerless)
+                expect(() => {
+                    definitionGenerator.createSecuritySchemes(mockServerless.service.custom.documentation.securitySchemes)
+                }).to.throw('oAuth2 implicit flow requires scopes')
+            });
+        });
+
+        describe('Multiple Schemes', () => {
+            it('should add an oauth2 and an apiKey security scheme to components', function() {
+                mockServerless.service.custom.documentation.securitySchemes = {
+                    'oAuth2_key': {
+                        type: 'oauth2',
+                        flows: {
+                            implicit: {
+                                authorizationUrl: 'http://example.org/api/oauth/dialog',
+                                scopes: {
+                                    'write:pets': 'modify pets in your account',
+                                    'read:pets': 'read your pets'
+                                }
+                            }
+                        }
+                    },
+                    'api_key': {
+                        type: 'apiKey',
+                        name: 'Authorization',
+                        in: 'header'
+                    }
+                }
+
+                const definitionGenerator = new DefinitionGenerator(mockServerless)
+                definitionGenerator.createSecuritySchemes(mockServerless.service.custom.documentation.securitySchemes)
+                console.log(definitionGenerator.openAPI.components.securitySchemes)
+                expect(definitionGenerator.openAPI).to.be.an('object')
+                expect(definitionGenerator.openAPI.components).to.be.an('object')
+                expect(definitionGenerator.openAPI.components).to.have.property('securitySchemes')
+                expect(definitionGenerator.openAPI.components.securitySchemes).to.be.an('object')
+                expect(definitionGenerator.openAPI.components.securitySchemes).to.have.property('oAuth2_key')
+                expect(definitionGenerator.openAPI.components.securitySchemes).to.have.property('api_key')
+            });
         });
     });
 
