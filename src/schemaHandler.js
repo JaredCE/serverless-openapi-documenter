b41921297fc65150be02a4998bff4e2342ad3b16
Jared Evans <jarede@gmail.com>
2023-05-21 10:09:14 +0100

    allow addition of schemas

--- src/schemaHandler.js
+++ src/schemaHandler.js
@@ -4,11 +4,15 @@ const path = require('path')
 
 const $RefParser = require("@apidevtools/json-schema-ref-parser")
 const SchemaConvertor = require('json-schema-for-openapi')
+const isEqual = require('lodash.isequal')
+const { v4: uuid } = require('uuid')
 
 class SchemaHandler {
     constructor(serverless, openAPI) {
         this.documentation = serverless.service.custom.documentation
         this.openAPI = openAPI
+
+        this.modelReferences = {}
 
         this.__standardiseModels()
 
@@ -48,19 +52,60 @@ class SchemaHandler {
 
             const dereferencedSchema = await this.__dereferenceSchema(modelSchema)
                 .catch(err => {
-                    // console.error(err)
+                    if(err.errors) {
+                        for (const error of err?.errors) {
+                            if (error.message.includes('HTTP ERROR')) {
+                                throw err
+                            }
+                        }
+                    }
                     return modelSchema
                 })
 
             const convertedSchemas = SchemaConvertor.convert(dereferencedSchema, modelName)
 
             for (const [schemaName, schemaValue] of Object.entries(convertedSchemas.schemas)) {
+                if (schemaName === modelName) {
+                    this.modelReferences[schemaName] = `#/components/schemas/${modelName}`
+                }
+
                 this.__addToComponents('schemas', schemaValue, schemaName)
             }
         }
     }
 
+    async createSchema(name, schema) {
+        let originalName = name;
+        let finalName = name;
 
+        if (this.modelReferences[name] && schema === undefined) {
+            return this.modelReferences[name]
+        }
+
+        const dereferencedSchema = await this.__dereferenceSchema(schema)
+            .catch(err => {
+                throw err
+            })
+
+        const convertedSchemas = SchemaConvertor.convert(dereferencedSchema, name)
+
+        for (const [schemaName, schemaValue] of Object.entries(convertedSchemas.schemas)) {
+            if (this.__existsInComponents(schemaName)) {
+                if (this.__isTheSameSchema(schemaValue, schemaName) === false) {
+                    if (schemaName === originalName) {
+                        finalName = `${schemaName}-${uuid()}`
+                        this.__addToComponents('schemas', schemaValue, finalName)
+                    } else {
+                        this.__addToComponents('schemas', schemaValue, schemaName)
+                    }
+                }
+            } else {
+                this.__addToComponents('schemas', schemaValue, schemaName)
+            }
+        }
+
+        return `#/components/schemas/${finalName}`
+    }
 
     async __dereferenceSchema(schema) {
         const bundledSchema = await $RefParser.bundle(schema, this.refParserOptions)
@@ -93,6 +138,31 @@ class SchemaHandler {
         return deReferencedSchema
     }
 
+    /**
+     * @function existsInComponents
+     * @param {string} name - The name of the Schema
+     * @returns {boolean} Whether it exists in components already
+     */
+    __existsInComponents(name) {
+        return Boolean(this.openAPI?.components?.schemas?.[name])
+    }
+
+    /**
+     * @function isTheSameSchema
+     * @param {object} schema - The schema value
+     * @param {string} otherSchemaName - The name of the schema
+     * @returns {boolean} Whether the schema provided is the same one as in components already
+     */
+    __isTheSameSchema(schema, otherSchemaName) {
+        return isEqual(schema, this.openAPI.components.schemas[otherSchemaName])
+    }
+
+    /**
+     * @function addToComponents
+     * @param {string} type - The component type
+     * @param {object} schema - The schema
+     * @param {string} name - The name of the schema
+     */
     __addToComponents(type, schema, name) {
         const schemaObj = {
             [name]: schema
